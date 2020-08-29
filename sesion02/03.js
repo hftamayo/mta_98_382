f0a9a59c556ee40bbd09f146ce01650b46b800bc
hftamayo <hftamayo@gmail.com>
2020-08-29 14:00:24 -0600

    archivos de trabajo sesion2

---
+++ sesion02/03.js
@@ -0,0 +1,8 @@
+var enteroNegativo = -3.14159265359;
+var primeraFrase =  "Esta es una cadena alfanumerica 45632 */+";
+var segundaFrase = "Podemos usar escape caracters:  \"texto entre comillas\".";
+var variableBoolean = true;
+var variableNula = null; //notese como será interpretada por el navegador
+var variableSinDefinir;
+
+// console.log(typeof nombreVariable);
