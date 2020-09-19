95bf2da164359bff52fbe6541eef087b95d217ec
hftamayo <hftamayo@gmail.com>
2020-09-19 07:58:31 -0600

    archivos sesion05

---
+++ sesion05/matematica.js
@@ -0,0 +1,25 @@
+function mostrarFunciones(cifra){
+    let dato = cifra;
+    //PI
+    Math.PI;
+    //redondear decimales
+    Math.round(dato);
+    //potenciacion, acepta params x, y
+    Math.pow(cifra, 2);
+    //raiz cuadrada
+    Math.sqrt(cifra);
+    //devuelve el valor absoluto (positivo)
+    Math.abs(cifra);
+    //devuelve el maximo numero posible a redondear
+    Math.ceil(cifra);
+    //devuelve el minimo numero posible a redondear
+    Math.floor(cifra);
+    //funcion seno
+    Math.sin(cifra);
+    //funcion coseno
+    Math.cos(cifra);
+    //generacion de numero aleatorio
+    Math.random();    
+    
+    
+}
