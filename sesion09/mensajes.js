d588dc2199ca5e5e5b9c86d9e4449c1ead9c49d7
Herbert Fernandez Tamayo <hftamayo@gmail.com>
2020-10-26 20:17:30 -0600

    archivos de trabajo sesion 9

---
+++ sesion09/mensajes.js
@@ -0,0 +1,17 @@
+
+function mensaje01() {
+    var mensaje = "Hola Mundo v1";
+    alert (mensaje);
+  }
+  
+  
+function mensaje02() {
+    var mensaje = "Hola Mundo v2;
+    alert (mensaje);
+  }  
+  
+
+funcion mensaje03() {
+    var mensaje = "Hola Mundo v3";
+    alert (mensaje);
+  }  
